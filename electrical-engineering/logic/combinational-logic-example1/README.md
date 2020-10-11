f583b2d69a235306f915152c968266ff4e671327
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2020-10-10 22:51:45 -0700

    adding logic gates

---
+++ electrical-engineering/logic/combinational-logic-example1/README.md
@@ -0,0 +1,26 @@
+# combinational-logic-example1
+
+Using
+[LaTeX](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/languages/latex-cheat-sheet/)
+to create a combinational logic image.
+
+## LATEX CODE
+
+Refer to
+[combinational-logic-example1.tex](combinational-logic-example1.tex)
+for the LaTeX code.
+
+## RUN
+
+This will crate the `.svg` image file from the `.tex` LaTeX code.
+
+```bash
+sh combinational-logic-example1.sh
+```
+
+## RESULTING IMAGE
+
+<p align="center">
+    <img src="combinational-logic-example1.svg"
+    align="middle"
+</p>
---
+++ electrical-engineering/logic/combinational-logic-example1/combinational-logic-example1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+# combinational-logic-example1.sh
+
+echo " "
+echo "************************************************************************"
+echo "****************************** combinational-logic-example1.sh (START) *"
+echo " "
+
+echo "Create .dvi file"
+echo "latex combinational-logic-example1.tex"
+latex combinational-logic-example1.tex
+echo " "
+
+echo "Convert .dvi to .svg"
+echo "    -n no fonts"
+echo "    -a This option forces dvisvgm to vectorize everything"
+dvisvgm -n -a combinational-logic-example1.dvi
+echo " "
+
+echo "******************************** combinational-logic-example1.sh (END) *"
+echo "************************************************************************"
+echo " "
