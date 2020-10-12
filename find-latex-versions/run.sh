288bb7a3771c7e83feaa44f6c1e9a17781f56b88
Jeff DeCola (addie) <jeffdecola@gmail.com>
2020-10-12 16:41:37 -0700

    added find latex versions

---
+++ find-latex-versions/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+# run.sh
+
+echo " "
+echo "************************************************************************"
+echo "******************************************************* run.sh (START) *"
+echo " "
+
+echo "Create .dvi file"
+echo "latex test.tex"
+latex test.tex
+echo " "
+
+echo "Convert .dvi to .svg"
+echo "    -n no fonts"
+echo "    -a This option forces dvisvgm to vectorize everything"
+dvisvgm -n -a -o test test.dvi
+echo " "
+
+echo "cat test.log | grep version"
+cat test.log | grep -E 'Package: |Version'
+echo " "
+
+echo "********************************************************* run.sh (END) *"
+echo "************************************************************************"
+echo " "
