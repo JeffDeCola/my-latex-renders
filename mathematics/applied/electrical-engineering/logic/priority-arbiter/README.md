3da08865fd306967187f29911101eabcc4bb9513
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2020-10-15 08:12:42 -0700

    adding priority arbiter

---
+++ mathematics/applied/electrical-engineering/logic/priority-arbiter/README.md
@@ -0,0 +1,33 @@
+# priority-arbiter
+
+Using
+[LaTeX](https://github.com/JeffDeCola/my-cheat-sheets/tree/master/software/development/languages/latex-cheat-sheet/)
+to create an AND logic gate.
+
+## LATEX CODE
+
+Using LaTeX package `tikz`.
+
+Refer to
+[priority-arbiter.tex](priority-arbiter.tex)
+for the LaTeX code.
+
+## RUN
+
+Run
+[run.sh](run.sh)
+to create an
+[priority-arbiter.svg](priority-arbiter.svg)
+image file from the
+[priority-arbiter.tex](priority-arbiter.tex) LaTeX code.
+
+```bash
+sh run.sh
+```
+
+## RESULTING .svg IMAGE
+
+<p align="center">
+    <img src="priority-arbiter.svg"
+    align="middle"
+</p>
---
+++ mathematics/applied/electrical-engineering/logic/priority-arbiter/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+# run.sh
+
+echo " "
+echo "************************************************************************"
+echo "******************************************************* run.sh (START) *"
+echo " "
+
+echo "Create .dvi file"
+echo "latex priority-arbiter.tex"
+latex priority-arbiter.tex
+echo " "
+
+echo "Convert .dvi to .svg"
+echo "    -n no fonts"
+echo "    -a This option forces dvisvgm to vectorize everything"
+dvisvgm -n -a -o priority-arbiter priority-arbiter.dvi
+echo " "
+
+echo "cp priority-arbiter.svg /mnt/c/Users/Jeff/home/jeff/systemverilog/my-systemverilog-examples/basic-synchronous-logic/priority-arbiter/svgs/."
+cp priority-arbiter.svg /mnt/c/Users/Jeff/home/jeff/systemverilog/my-systemverilog-examples/basic-synchronous-logic/priority-arbiter/svgs/.
+echo " "
+
+echo "********************************************************* run.sh (END) *"
+echo "************************************************************************"
+echo " "
