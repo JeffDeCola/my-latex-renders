c1e9a3b382c760cb480855e758cb34173ea27de8
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2023-05-05 16:59:25 -0400

    added 74x151

--- mathematics/applied/electrical-engineering/combinational-logic/74x151-multiplexer/run.sh
+++ mathematics/applied/electrical-engineering/combinational-logic/74x151-multiplexer/run.sh
@@ -7,18 +7,18 @@ echo "******************************************************* run.sh (START) *"
 echo " "
 
 echo "Create .dvi file"
-echo "latex mux-4x1.tex"
-latex mux-4x1.tex
+echo "latex 74x151-multiplexer.tex"
+latex 74x151-multiplexer.tex
 echo " "
 
 echo "Convert .dvi to .svg"
 echo "    -n no fonts"
 echo "    -a This option forces dvisvgm to vectorize everything"
-dvisvgm -n -a -o mux-4x1 mux-4x1.dvi
+dvisvgm -n -a -o 74x151-multiplexer 74x151-multiplexer.dvi
 echo " "
 
-echo "cp mux-4x1.svg ~/verilog/my-verilog-examples/combinational-logic/multiplexers-and-demultiplexers/mux_4x1/svgs/."
-cp mux-4x1.svg ~/verilog/my-verilog-examples/combinational-logic/multiplexers-and-demultiplexers/mux_4x1/svgs/.
+echo "cp 74x151-multiplexer.svg ~/verilog/my-verilog-examples/combinational-logic/multiplexers-and-demultiplexers/jeff_74x151/svgs/."
+cp 74x151-multiplexer.svg ~/verilog/my-verilog-examples/combinational-logic/multiplexers-and-demultiplexers/jeff_74x151/svgs/.
 echo " "
 
 echo "********************************************************* run.sh (END) *"
